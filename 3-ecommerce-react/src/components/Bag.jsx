2afa81e05c1956a08e99f01f0db87297a8c54db2
Mohit Sharma <mohitsharma7258@gmail.com>
2025-02-20 13:01:09 +0530

    bag

---
+++ 3-ecommerce-react/src/components/Bag.jsx
@@ -0,0 +1,19 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Bag = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="bag-page">
+          <div className="bag-items-container"></div>
+          <div className="bag-summary"></div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Bag;
