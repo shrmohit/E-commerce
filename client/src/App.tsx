803a6470ba1880582d91733729f7ad74e59aa847
mohit sharma <mohitsharma7258@gmail.com>
2025-03-05 19:54:17 +0530

    router in cart ,home,seacrh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+import Home from "./pages/home";
+import Search from "./pages/search";
+import Cart from "./pages/cart";
 
 function App() {
   return (
-    <>
-      <h1>mohit</h1>
-    </>
+    <Router>
+      {/* Header */}
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        ></Route>
+        <Route
+          path="/Search"
+          element={<Search />}
+        ></Route>
+        <Route
+          path="/Cart"
+          element={<Cart />}
+        ></Route>
+      </Routes>
+    </Router>
+    // <>
+    //   <h1>mohit</h1>
+    // </>
   );
 }
 
