40b588c63532cae302cddd7052d08bd432e6fb87
mohit sharma <mohitsharma7258@gmail.com>
2025-03-10 02:07:10 +0530

    hero

--- frontend/src/components/Layout/Hero.jsx
+++ frontend/src/components/Layout/Hero.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import heroImage from "../../assets/rabbit-hero.webp";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="relative">
+      <img
+        src={heroImage}
+        alt="Rabbit"
+        className="w-full h-[400px] md:h-[600px] lg:h-[750px] object-cover"
+      />
+      <div className="absolute inset-0  bg-black bg-opacity-5 flex items-center justify-center">
+        <div className="text-center text-white p-6">
+          <h1 className="text-4xl md:text-9xl font-bold tracking-tighter uppercase mb-4">
+            Vacation
+            <br /> Ready
+          </h1>
+          <p className="text-sm tracking-tighter md:text-lg mb-6">
+            Explore our vacation-ready outfits with fast worlwide shipping.
+          </p>
+          <Link
+            to="#"
+            className="bg-white text-gray-950 px-6 py-2 rounded-sm text-lg"
+          >
+            Shop Now
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
