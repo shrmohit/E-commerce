d7b809fb2fe10d70213fddc915c807823e200063
mohit sharma <mohitsharma7258@gmail.com>
2025-03-10 02:38:45 +0530

    womens and mens collection

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Layout/Hero";
+import GenderCollection from "../components/Products/GenderCollection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <GenderCollection />
     </div>
   );
 };
