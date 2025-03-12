226b901ae399e63cc5093af6c9ebf67a9567120a
mohit sharma <mohitsharma7258@gmail.com>
2025-03-12 23:29:47 +0530

    strcture created

---
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <div>
+      <div>
+        {/* two image select */}
+        <div>
+          <img
+            src=""
+            alt=""
+          />
+          <img
+            src=""
+            alt=""
+          />
+        </div>
+        {/* show one big image */}
+        <div></div>
+        {/* detail of image products */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Layout/Hero";
 import GenderCollection from "../components/Products/GenderCollection";
 import NewArrivals from "../pages/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <NewArrivals />
       {/* Best seller */}
       <h2 className="text-center font-bold mb-4 text-3xl">Best Seller</h2>
+      <ProductDetails />
     </div>
   );
 };
