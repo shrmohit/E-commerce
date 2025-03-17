d931d5037425eb1648318bf2cadcf141a0b37856
mohit sharma <mohitsharma7258@gmail.com>
2025-03-17 23:45:21 +0530

    product detail

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 const selectedProduct = {
-  name: 120,
+  name: "Stylish Jacket",
+  price: 120,
   originalPrice: 150,
-  Description: "This is a stylish Jacket perfect for any ocasion",
+  Description:
+    "A slim-fit easy-iron shirt in women cotton fabric with a fitted " +
+    "silhouette. Feature a turn-dowm collar, classic button " +
+    "placket,and a yoke at the back. Long sleeves and adjustable " +
+    "button cuffs with a rounded hem.",
   brand: "FshionBrand",
   material: "Leather",
-  sizes: ["5", "M", "L", "XL"],
-  color: ["Red", "Black"],
+  sizes: ["S", "M", "L", "XL"],
+  colors: ["Red", "Black"],
   images: [
     {
       url: "https://picsum.photos/500/500/?random=1",
@@ -47,34 +52,94 @@ const ProductDetails = () => {
             </div>
           </div>
           {/*Mobile Thumbnail  */}
+          <div className="md:hidden flex overscroll-x-scroll space-x-4 mb-4">
+            {selectedProduct.images.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={image.altText || `Thumbnail ${index}`}
+                className="w-20 h-20 object-cover rounded-lg curser-pointer border"
+              />
+            ))}
+          </div>
           <div>
-            <div className="">
-              <h3>Slim-Fit Easy Iron Shirt</h3>
-              <p>$ 34.99</p>
-              <p>
-                A slim-fit easy-iron shirt in women cotton fabric with a fitted
-                silhouette. Feature a turn-dowm collar, classic button
-                placket,and a yoke at the back. Long sleeves and adjustable
-                button cuffs with a rounded hem.
+            <div className="md:w-1/2 md:ml-10">
+              <h1 className="text-2xl md:text-3xl font-semibold mb-2">
+                {selectedProduct.name}
+              </h1>
+              <p className="text-lg text-gray-600 mb-1 line-through">
+                {selectedProduct.originalPrice &&
+                  `${selectedProduct.originalPrice}`}
               </p>
-              <div>
-                <p>Color:</p>
+              <p className="text-xl text-gray-600 mb-4">
+                $ {selectedProduct.price}
+              </p>
+              <p className="text-gray-600 mb-4">
+                {selectedProduct.Description}
+              </p>
+              <div className="mb-4">
+                <p className="text-gray-700">Color:</p>
+                <div className="flex gap-2 mt-2">
+                  {selectedProduct.colors.map((color) => (
+                    <button
+                      key={color}
+                      className="w-8 h-8 rounded-full border"
+                      style={{
+                        backgroundColor: color.toLocaleLowerCase(),
+                        filter: "brightness(0.5)",
+                      }}
+                    ></button>
+                  ))}
+                </div>
               </div>
               <div>
-                <p>Size:</p>
+                <p className="text-gray-700 ">Size:</p>
+                <div className="flex gap-2 mt-2">
+                  {selectedProduct.sizes.map((size) => (
+                    <button
+                      key={size}
+                      className="px-4 py-2 border font-bold rounded-sm shadow-sm"
+                    >
+                      {size}
+                    </button>
+                  ))}
+                </div>
               </div>
-              <div>
-                <p>Quantity</p>
+              <div className="mt-2">
+                <p className="text-gray-700">Quantity:</p>
+                <div
+                  className="flex
+                mt-2 gap-2"
+                >
+                  <button className="bg-gray-400 h-8 w-6 rounded-sm font-bold">
+                    -
+                  </button>
+                  <span className="text-center">1</span>
+                  <button className="bg-gray-400 h-8 w-6 rounded-sm font-bold">
+                    +
+                  </button>
+                </div>
               </div>
-              <button>ADD TO CART</button>
-              <div>
-                <p>Charateristics</p>
-                <ul>
-                  <li>Brand</li>
-                  <li>Material</li>
-                  <li>Urban Chic</li>
-                  <li>Cotton</li>
-                </ul>
+              <button
+                className="py-5 w-full bg-black text-white rounded
+               mt-5"
+              >
+                ADD TO CART
+              </button>
+              <div className="mt-10 text-gray-700 ">
+                <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
+                <table className="w-full text-left text-sm text-gray-600">
+                  <tbody>
+                    <tr>
+                      <td className="py-1 ">Brand</td>
+                      <td className="py-1">{selectedProduct.brand}</td>
+                    </tr>
+                    <tr>
+                      <td>Material</td>
+                      <td>{selectedProduct.material}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
