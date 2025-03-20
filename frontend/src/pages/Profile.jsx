a76883e0b21d260083f8f87be46a19e53e23799a
mohit sharma <mohitsharma7258@gmail.com>
2025-03-20 09:45:53 +0530

    profile -left section

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="min-h-screen flex flex-col ">
+      <div className="flex-grow container mx-auto p-4 md:p-6">
+        <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 ">
+          {/* left section */}
+          <div className="w-full md:w-1/3 lg:w-1/4 shadow-md rounded-lg p-6">
+            <h2 className="text-2xl lg:text-3xl font-bold mb-4">John Doe</h2>
+            <p className="text-lg text-gray-600 mb-4">John@example.com</p>
+            <button
+              className="w-full text-center
+             bg-red-500 text-white rounded  py-2 px-4 hover:bg-red-600"
+            >
+              Logout
+            </button>
+
+            {/* right section - order table
+             */}
+            <div className=""></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
