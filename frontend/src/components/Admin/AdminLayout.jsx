a543af542f9ef7e7676c4ef4fad8f68b43fcd5d3
mohit sharma <mohitsharma7258@gmail.com>
2025-03-23 12:26:01 +0530

    admin navbar

--- frontend/src/components/Admin/AdminLayout.jsx
+++ frontend/src/components/Admin/AdminLayout.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 const AdminLayout = () => {
-  return <div>AdminLayout</div>;
+  const [isSidebarOpen, setIsSideOpen] = useState(false);
+
+  const handleSidebar = () => {
+    setIsSideOpen(!isSidebarOpen);
+  };
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row relative">
+      {/* MObile toggle button */}
+      <div className="flex md:hidden bg-gray-900 p-4 text-white z-20">
+        <button>
+          <FaBars />
+        </button>
+      </div>
+
+      {/* <div className="w-64 bg-black h-screen flex ">L</div>
+      <div>R</div> */}
+    </div>
+  );
 };
 
 export default AdminLayout;
