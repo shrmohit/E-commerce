5b31eb6b1a3c65f3e45218cba64542bade6f17a2
mohit sharma <mohitsharma7258@gmail.com>
2025-03-23 22:08:14 +0530

    productmanagement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
 
 function App() {
   return (
@@ -77,6 +78,10 @@ function App() {
             <Route
               path="user"
               element={<UserManagement />}
+            />
+            <Route
+              path="products"
+              element={<ProductManagement />}
             />
           </Route>
         </Routes>
