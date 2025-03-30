fe352ab74d993226857ecf6b2eafae8c7f4f03ba
mohit sharma <mohitsharma7258@gmail.com>
2025-03-30 21:57:01 +0530

    register

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
-
 const router = express.Router();
 
 // @route POST /api/users/register
@@ -19,14 +18,74 @@ router.post("/register", async (req, res) => {
     user = new User({ name, email, password });
     await user.save();
 
-    res.status(201).json({
-      user: {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      },
-    });
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //send user and token response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route POST /api/users/login
+//@desc Authenticate user
+//@access Public
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    //find the user by email
+    let user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+
+    const isMatch = await user.matchPassword(password);
+    // bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid Credentials" });
+
+    // Create JWT Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "40h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //send user and token response
+        res.json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).send("Server Error");
