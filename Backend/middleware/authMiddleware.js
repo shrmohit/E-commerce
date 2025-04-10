891a3d79c00a129027da44b104aaa3ffc7854e42
mohit sharma <mohitsharma7258@gmail.com>
2025-04-10 22:01:04 +0530

    error

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -24,4 +24,13 @@ const protect = async (req, res, next) => {
   }
 };
 
-module.exports = { protect };
+// Middleware to protect admin routes
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(403).json({ message: "Not authorized,token failed" });
+  }
+};
+
+module.exports = { protect, admin };
--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const Product = require("../models/product");
-const { protect } = require("../middleware/authMiddleware");
-const product = require("../models/product");
+const { protect, admin } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 //@route POST /api/product
 //@desc Create a new product
 //@access Private/Admin
-router.post("/", protect, async (req, res) => {
+router.post("/", protect, admin, async (req, res) => {
   try {
     const {
       name,
@@ -52,11 +51,78 @@ router.post("/", protect, async (req, res) => {
       dimension,
       weight,
       sku,
-      user: req.user._id,
+      user: req.user._id, // Assuming you want to associate the product with the user who created it
     });
 
     const createdProduct = await newProduct.save();
     res.status(201).json(createdProduct);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route GET /api/product/:id
+//@desc Update an existing product ID
+//@access Private/Admin
+
+router.get("/:id", protect, admin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublised,
+      tags,
+      dimension,
+      weight,
+      sku,
+    } = req.body;
+
+    //find product by ID
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      //update product fields
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      //isFeatured and isPublised are boolean values, so we need to check if they are defined before updating
+      product.isFeatured =
+        isFeatured !== undefined ? isFeatured : product.isFeatured;
+      product.isPublised =
+        isPublised !== undefined ? isPublised : product.isPublised;
+      product.tags = tags || product.tags;
+      product.dimension = dimension || product.dimension;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      //save the updated product
+      const updatedProduct = await product.save();
+      res.status(200).json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
